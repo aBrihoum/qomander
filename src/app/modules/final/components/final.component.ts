1a424d2d71f62a508761a17a880f165cd1b111c6
Abdjalil Brihoum <ou.brihoum@gmail.com>
2023-03-09 03:26:38 +0100

    add ability to scroll by mousewheel

--- src/app/modules/final/components/final.component.ts
+++ src/app/modules/final/components/final.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MealListT } from '../../shared/services/meal-list/meal-list.model';
 import { MealOptionsT } from '../../shared/services/meal-options/meal-options.model';
 import { MealOptionsService } from '../../shared/services/meal-options/meal-options.service';
@@ -15,10 +15,15 @@ export class FinalComponent {
   selectedOptions: MealOptionsT[] = this.MealOptionsService.returnSelectedMealOptions();
   optionsTotalPrice: number = this.MealOptionsService.returnSelectedMealOptionsTotalPrice();
   selectedMealQuantity = this.MealOptionsService.returnSelectedMealQuantity();
-
+  @ViewChild('optionsContainer') optionsContainer!: ElementRef<HTMLDivElement>;
   reset() {
     //sorry, too lazy to reset all the already modified vars, so i just refresh
     this.route.navigate(['/menu']).then(() => window.location.reload());
+  }
+
+  scrollEvent($event: WheelEvent) {
+    let el = this.optionsContainer.nativeElement;
+    el.scrollLeft += $event.deltaY < 0 ? -100 : 100;
   }
 
   ngOnInit() {
