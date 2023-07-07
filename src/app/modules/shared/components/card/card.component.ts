325ab0dd4029082419bf2afd1b59e7e214ed23be
Abdjalil Brihoum <abdjalil@brihoum.com>
2023-07-07 14:50:50 +0100

    remove unused import

--- src/app/modules/shared/components/card/card.component.ts
+++ src/app/modules/shared/components/card/card.component.ts
@@ -1,6 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MealListT } from '../../services/meal-list/meal-list.model';
-import { MealOptionsT } from '../../services/meal-options/meal-options.model';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
